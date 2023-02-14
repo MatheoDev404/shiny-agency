6736b2b5d9ce7421b1600e71b468d62ab09e0f83
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2023-02-14 14:20:00 +0100

    initialisation routage

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Route path="/">
+                <Home />
+            </Route>
+            <Route path="/survey">
+                <Survey />
+            </Route>
+        </Router>
+    <React.StrictMode>
+)
