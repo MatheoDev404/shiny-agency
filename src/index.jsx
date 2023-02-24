5394415ef591b31b13bb63e5e6e992bd3063fbc9
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2023-02-24 12:02:25 +0100

    ajout pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
- 
-ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <Route path="/">
-                <Home />
-            </Route>
-            <Route path="/survey">
-                <Survey />
-            </Route>
-        </Router>
-    <React.StrictMode>
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Error from './components/Error'
+import Freelances from './pages/Freelances'
+import Results from './pages/Results'
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/survey/:questionNumber" element={<Survey />}></Route>
+        <Route path="/freelances" element={<Freelances />}></Route>
+        <Route path="/results" element={<Results />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
